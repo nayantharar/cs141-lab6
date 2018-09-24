57c2cb7ce29ee164fc25e70ef642a29ac61daaf2
nayantharar <42238330+nayantharar@users.noreply.github.com>
2018-09-24 09:13:08 +0530

    Update lab6_q7.cpp

--- lab6_q7.cpp
+++ lab6_q7.cpp
@@ -1,24 +1,38 @@
+//include library
 #include<iostream>
 using namespace std;
 
+//main function
 char toupper(char lowercase,char uppercase)
-{uppercase=lowercase-32;
-return uppercase;}
+{
+  uppercase=lowercase-32;
+  return uppercase;
+}
 
 char tolower(char uppercase,char lowercase)
-{lowercase=uppercase+32;
-return lowercase;}
+{
+  lowercase=uppercase+32;
+  return lowercase; 
+}
 
 
 
-int main(){
-char ch,alp,result;
-cout<<"enter any alphabet"<<endl;
-cin>>ch;
-if(ch>='a' &&ch<='z')
-{result=toupper(ch,alp);
-cout<<"uppercase character is"<<result<<endl;}
-else
-{result= tolower(ch,alp);
-cout<<"lowercase character is"<<result<<endl;}
-return 95;}
+int main()
+{
+     char ch,alp,result;
+     cout<<"enter any alphabet"<<endl;
+     cin>>ch;
+     if(ch>='a' &&ch<='z')
+    {
+       result=toupper(ch,alp);
+       cout<<"uppercase character is"<<result<<endl; 
+     }
+  else
+  {
+    result= tolower(ch,alp);
+    cout<<"lowercase character is"<<result<<endl;
+   }
+ 
+//end program
+return 0;
+}
