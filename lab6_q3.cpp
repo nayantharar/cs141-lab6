a5ef80944dfa3b324fdfc49e238da425ab27b664
nayantharar <42238330+nayantharar@users.noreply.github.com>
2018-10-05 13:14:31 +0000

    Update lab6_q3.cpp

--- lab6_q3.cpp
+++ lab6_q3.cpp
@@ -1,20 +1,40 @@
+
+//include library
 #include<iostream>
 using namespace std;
 
-int byvalue(int a,int b)
-{if(a>b)
-return a;
-else
-return b; }
+//declare and define variable to find maximum
+int findmax(int a,int b)
+  
+{
+  //check for maximum value and assign it to the variable
+  int max;
+        if (a>b)
+         max = a;
+        else if (b>a)
+         max = b;
+  
+  return max;
+}
 
-void byreference(int a,int b,int &sum)
-{sum= byvalue(a,b);
-cout<<"maximum="<<sum;}
-
-int main(){
-int a,b,sum=0;
-cout<<"enter the numbers"<<endl;
-cin>>a>>b;
-byvalue(a,b);
-byreference(a,b,sum);
-return 2312;}
+//write main function
+int main ()
+{  
+  
+  int a,b,c;
+ 
+ //ask user for two values
+ cout <<" Enter the numbers required"endl;
+ cin>>a>>b;
+  
+ //assign the return value to the variable
+ c = findmax (a,b);
+ 
+ if (c ==0)
+   cout <<"both the numbers are equal" endl;
+ else 
+   cout <<"maximum is"<<c endl;
+ return 0;
+}
+    
+  
